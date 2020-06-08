418e7a32982c45e3f5ff800429e74b56ddea5f3f
Hafidh <hafidh1bridge@gmail.com>
2020-06-08 08:23:26 +0530

    Fixed winning bug

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,65 +3,72 @@ def create_board(board):
     row1 = "\t|{0}|{1}|{2}|".format(board[0], board[1], board[2])
     row2 = "\t|{0}|{1}|{2}|".format(board[3], board[4], board[5])
     row3 = "\t|{0}|{1}|{2}|".format(board[6], board[7], board[8])
-    board = " \n{}\t\t\t|1|2|3|\n\t-------\t\t\t-------\n{}\t\t\t|4|5|6|\n\t-------\t\t\t--------\n{}\t\t\t|7|8|9|".format(row1, row2, row3)
+    board = " \n{}\t\t\t|1|2|3|\n\t-------\t\t\t-------\n{}\t\t\t|4|5|6|\n\t-------\t\t\t--------\n{}\t\t\t|7|8|9|".format(
+        row1, row2, row3)
     return board
 
-
-
-def update_board(bd,p1=" ",p2=" "):
-    if bd[p1-1]==" ":
-        bd[p1-1]="X"
-    elif bd[p2-1]==" ":
-        bd[p2-1]="O"
+def update_board(bd, p1=" ", p2=" "):
+    if bd[p1-1] == " ":
+        bd[p1-1] = "X"
+    elif bd[p2-1] == " ":
+        bd[p2-1] = "O"
     else:
         pass
     return print(create_board(bd))
 
-
 def win_check(bd):
+    i,j="X","O"
     #row checking
-    for i in [0,3,6]:
-        if ((bd[i] == "X" and bd[i+1] == "X" and bd[i+2] == "X") 
-        or (bd[i] == "O" and bd[i+1] == "O" and bd[i+2] == "O")):
-            return True
+    if ((bd[0] == i and bd[1] == i and bd[2] == i) or \
+        (bd[3] == i and bd[4] == i and bd[5] == i) or \
+            (bd[6] == i and bd[7] == i and bd[8] == i) or \
+                (bd[0] == i and bd[3] == i and bd[6] == i) or \
+                    (bd[1] == i and bd[4] == i and bd[7] == i) or \
+                        (bd[2] == i and bd[5] == i and bd[8] == i) or \
+                            (bd[0] == i and bd[4] == i and bd[8] == i) or \
+                                (bd[2] == i and bd[4] == i and bd[6] == i)):
+                                return True
+    elif ((bd[0] == j and bd[1] == j and bd[2] == j) or\
+        (bd[3] == j and bd[4] == j and bd[5] == j) or\
+            (bd[6] == j and bd[7] == j and bd[8] == j) or\
+                (bd[0] == j and bd[3] == j and bd[6] == j) or\
+                    (bd[1] == j and bd[4] == j and bd[7] == j) or\
+                        (bd[2] == j and bd[5] == j and bd[8] == j) or\
+                            (bd[0] == j and bd[4] == j and bd[8] == j) or\
+                                (bd[2] == j and bd[4] == j and bd[6] == j)):
+                                return True
+    else:
+        return False
 
-    #diag checking
-    for i in [0,2]:
-        if ((bd[i] == "X" and bd[i+4] == "X" and bd[i+8] == "X")
-                or (bd[i] == "O" and bd[i+4] == "O" and bd[i+8] == "O")):
-            return True
-        elif ((bd[i] == "X" and bd[i+2] == "X" and bd[i+4] == "X")
-                or (bd[i] == "O" and bd[i+2] == "O" and bd[i+4] == "O")):
-            return True
 
-    #col checking
-    for i in [0,1,2]:
-        if ((bd[i] == "X" and bd[i+3] == "X" and bd[i+6] == "X")
-                or (bd[i] == "O" and bd[i+3] == "O" and bd[i+6] == "O")):
-            return True
 
 board = [" " for _ in range(9)]
+
 
 def main():
     p1 = 'player 1'
     p2 = 'player 2'
     print('{} - (X)  {} - (O)'.format(p1, p2))
     print(create_board(board))
-    gameover=False
+    gameover = False
     while not gameover:
-        ip1,ip2=" "," "
-        while ip1==" " or 0 <= ip1 or ip1 > 9:
-            ip1 = int(input("Enter the place no for inserting(X): "))
-            print("Enter a valid place!!!" ) if 0 <= ip1 or ip1 > 9 else update_board(board,ip1,ip2)
-        if win_check(board)==True: 
-            gameover=True
-            print(p1," Wins !!!!")
+        ip1, ip2 = " ", " "
+        while ip1 == " ":
+            ip1 = int(input("Enter the place no for inserting(X): ").strip())
+            print("Enter a valid place!!!") if ip1 <= 0 or ip1 > 9 else update_board(
+                board, ip1, ip2)
+        if win_check(board) == True:
+            gameover = True
+            print(p1, " Wins !!!!")
             break
-        while ip2==" " or 0 <= ip2 or ip2 > 9:
-            ip2 = int(input("Enter the place no for inserting(O): "))
-            print("Enter a valid place!!!" ) if 0 <= ip2 or ip2 > 9 else update_board(board,ip1,ip2)
+        while ip2 == " ":
+            ip2 = int(input("Enter the place no for inserting(O): ").strip())
+            print("Enter a valid place!!!") if ip2 <= 0 or ip2 > 9 else update_board(
+                board, ip1, ip2)
         if win_check(board) == True:
             gameover = True
             print(p2, " Wins !!!!")
             break
+
+
 main()
